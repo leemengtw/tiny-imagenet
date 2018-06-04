a067d2df7c9060182b1c92adfd4691a4325de353
leemengtaiwan <b98705001@gmail.com>
2018-06-04 16:17:44 +0900

    Add __len__

--- tiny_imagenet.py
+++ tiny_imagenet.py
@@ -23,11 +23,30 @@ class TinyImageNet(data.Dataset):
 
     def __len__(self):
         """Denotes the total number of samples"""
-        return len(self.list_IDs)
+        return len(self.subset.samples)
 
     def __getitem__(self, index):
         """Generates one sample of data"""
+        return 'I dont know'
+
 
 if __name__ == '__main__':
-    tiny_train = TinyImageNet('./dataset', subset='train')
-    print(tiny_train)
+
+
+    import torchvision.transforms as transforms
+    transform = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+
+
+    tiny_test = TinyImageNet('./dataset', subset='test', transform=transform)
+    print(tiny_test.__len__())
+
+    import torch
+    loader = torch.utils.data.DataLoader(tiny_test, batch_size=128,
+                                         shuffle=True, num_workers=4)
+    dataiter = iter(loader)
+    images, labels = dataiter.next()
