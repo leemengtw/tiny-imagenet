76fdaa11c859516a312ebb017b75119b3d53f20b
leemengtaiwan <b98705001@gmail.com>
2018-06-04 23:14:33 +0900

    Implement ImageFolder like function

--- tiny_imagenet.py
+++ tiny_imagenet.py
@@ -1,52 +1,45 @@
 import os
 import os.path
+import glob
 import torch.utils.data as data
-import torchvision.datasets as datasets
+from PIL import Image
 
 
 class TinyImageNet(data.Dataset):
-    """
+    """Tiny ImageNet data set available from `http://cs231n.stanford.edu/tiny-imagenet-200.zip`.
+
     Parameters
     ----------
     root: string
-        Root directory including `train`, `test` and `valid`.
+        Root directory including `train`, `test` and `valid` subdirectories.
     subset: string
-        `train`, `test`, `valid`
+        Indicating which subset(split) to return as a data set.
+        Valid option: [`train`, `test`, `valid`]
+    transform: torchvision.transforms
+        A (series) of valid transformation(s).
     """
-
     def __init__(self, root, subset='train', transform=None):
         self.root = os.path.expanduser(root)
         self.subset = subset
         self.transform = transform
         self.subset_dir = os.path.join(root, self.subset)
-        self.subset = datasets.ImageFolder(self.subset_dir, self.transform)
+        self.image_paths = sorted(glob.iglob(self.subset_dir + '/**/*.JPEG', recursive=True))
 
     def __len__(self):
-        """Denotes the total number of samples"""
-        return len(self.subset.samples)
+        return len(self.image_paths)
 
     def __getitem__(self, index):
-        """Generates one sample of data"""
-        return 'I dont know'
+        img = Image.open(self.image_paths[index])
+        img = self.transform(img) if self.transform else img
+
+        return 
 
 
 if __name__ == '__main__':
 
-
-    import torchvision.transforms as transforms
-    transform = transforms.Compose([
-        transforms.Resize(32),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    tiny_train = TinyImageNet('./dataset', subset='train')
+    print(len(tiny_train))
+    print(tiny_train.__getitem__(99999))
 
 
 
-    tiny_test = TinyImageNet('./dataset', subset='test', transform=transform)
-    print(tiny_test.__len__())
-
-    import torch
-    loader = torch.utils.data.DataLoader(tiny_test, batch_size=128,
-                                         shuffle=True, num_workers=4)
-    dataiter = iter(loader)
-    images, labels = dataiter.next()
