6dc927cb72e2955cd8f8c10ea202e76417e38558
mnicnc404 <mnicnc404@gmail.com>
2018-07-18 09:14:08 +0800

    reinfe net struct

--- NaiveResNet.py
+++ NaiveResNet.py
@@ -1,25 +1,32 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride):
+
+    def __init__(self, in_channels, out_channels, stride, t=2):
         super().__init__()
         # compute Z[L+2]
+        mid_channels = in_channels * t
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=stride, padding=1, groups=in_channels),
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
+            nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=mid_channels, out_channels=mid_channels, kernel_size=3, stride=stride, padding=1, groups=mid_channels),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=mid_channels, out_channels=out_channels, kernel_size=1),
             nn.BatchNorm2d(out_channels)
         )
-        self.relu = nn.ReLU(inplace=True)
+        # self.relu = nn.ReLU(inplace=True)
 
         # downsample a[L] in case there is stride in conv1
         if stride != 1:
+            assert stride == 2
             self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(out_channels)
-            )
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1),
+                nn.BatchNorm2d(out_channels))
         else:
             self.downsample = None
 
@@ -27,10 +34,11 @@ class ResidualBlock(nn.Module):
         identity = x.clone()
         if self.downsample:
             identity = self.downsample(identity)
-        return self.relu(self.conv(x) + identity)
+        return self.conv(x) + identity
 
 
 class GlobalAveragePooling(nn.Module):
+
     def __init__(self):
         super().__init__()
 
@@ -39,6 +47,7 @@ class GlobalAveragePooling(nn.Module):
 
 
 class NaiveResNet(nn.Module):
+
     def __init__(self, num_classes):
         super().__init__()
         self.groups = nn.ModuleList([
@@ -71,8 +80,8 @@ class NaiveResNet(nn.Module):
 
 
 if __name__ == '__main__':
-
-    dummy_input = Variable(torch.rand(16, 3, 64, 64))
-    resnet = NaiveResNet(num_classes=200)
-    y = resnet.forward(dummy_input)
-    print(y.size())
+    with torch.no_grad():
+        dummy_input = torch.rand(16, 3, 64, 64)
+        resnet = NaiveResNet(num_classes=200)
+        y = resnet.forward(dummy_input)
+        print(y.size())
